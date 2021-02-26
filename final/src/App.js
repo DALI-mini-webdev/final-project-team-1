41b5bb30d06f31ca5bafafb1f19ce85af8fb843a
gracewanggw <grace.wang.24@dartmouth.edu>
2021-02-26 13:40:21 -0600

    styling of background and navbar

--- final/src/App.js
+++ final/src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import './App.css';
-import NavBar from ".components/Navbar/NavBar";
+import NavBar from "./components/Navbar/NavBar";
 
 function App() {
   return (
     <div className="App">
-      hi
       <NavBar />
-       
-y
+      <body className="page">
+        Welcome!
+      </body>
     </div>
   );
 }
