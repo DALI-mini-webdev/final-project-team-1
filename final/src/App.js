2c85f8e5a67f681a41f850b67b9689d1776cd4ec
clairemckenna <claire.l.mckenna.23@dartmouth.edu>
2021-02-26 09:52:26 -0500

    newReact

---
+++ final/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import NavBar from ".components/Navbar/NavBar";
+
+function App() {
+  return (
+    <div className="App">
+      hi
+      <NavBar />
+       
+y
+    </div>
+  );
+}
+
+export default App;
---
+++ final/src/components/Navbar/MenuItems.js
@@ -0,0 +1,31 @@
+export const MenuItems = [
+    {
+        title: 'Athletics',
+        url: '#',
+        cName: 'nav-links'
+    },
+
+    {
+        title: 'Academics',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Admissions',
+        url: '#',
+        cName: 'nav-links'
+    },
+
+    {
+        title: 'Community',
+        url: '#',
+        cName: 'nav-links'
+    },
+
+    {
+        title: 'Other',
+        url: '#',
+        cName: 'nav-links'
+    }
+
+]
