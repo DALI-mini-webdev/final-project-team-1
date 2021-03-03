22c5bee9ee852853c44d4ef77b7debb8ae0fcc6b
isabellekitchel <isabelle.h.kitchel.23@dartmouth.edu>
2021-03-03 17:38:19 -0500

    contact page

--- final/src/pages/ContactUs.js
+++ final/src/pages/ContactUs.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import Form from '../components/Form.js';
+import '../components/Form.css'
 
 function ContactUs(){
 
+const contactList = ["610-213-2922", "[email]"];
+const contactMap = contactList.map((method) => {
+    return(
+    <p className="green-text">{method}</p>
+    )
+});
 
 return(
     <div>
         <h1>Contact US!!!</h1>
-        <Form type='contact us'/>
+        <div>
+            <p className="green-text">Our information:</p>
+            <div>{siblingsMap}</div>
+        </div>
     </div>
 );
 
